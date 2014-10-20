b86595b5d47aee0aac8e637ee3270cd6eee95ec7
srekargithub <sreekar3082@gmail.com>
2014-10-20 17:03:12 +0530

    BDE changes done

--- com/nexii/lead/BDE.java
+++ com/nexii/lead/BDE.java
@@ -1,5 +1,7 @@
 package com.nexii.lead;
 
+import java.util.List;
+
 import org.testng.annotations.Test;
 
 public class BDE extends Helper {
@@ -12,7 +14,33 @@ public class BDE extends Helper {
 
 	 // helper.login_CRM();
 
-	  //Sreekar, continue the code here
+	  //Sreekar, continue the code heressss
+	  @Test   
+		public void aBDM_test_expand_close_menu() 
+		{
+		  List<WebElement> lis = driver.findElement(By.className("menu")).findElements(By.className("close"));
+		  helper.sleep(3);
+		  System.out.println(lis.size());
+		  helper.sleep(1);
+		  for (int i = 0; i < lis.size(); i++)
+		  {
+		    System.out.println("Expanding::"+lis.get(i).getText());
+			lis.get(i).findElement(By.className("    symbol-close")).click();
+			helper.sleep(1);
+		  }
+		  helper.sleep(3);
+		  List<WebElement> lis2 = driver.findElement(By.className("menu")).findElements(By.className("open"));
+		  helper.sleep(1);
+		  System.out.println(lis2.size());
+		  helper.sleep(1);
+		  for (int i = 0; i < lis2.size(); i++)
+		  {
+		    System.out.println("Closing::"+lis2.get(i).getText());
+			lis.get(i).findElement(By.className("     symbol-open")).click();
+			helper.sleep(1);
+		  }  
+		  driver.close();
+	    }
 	  
   }
 }
